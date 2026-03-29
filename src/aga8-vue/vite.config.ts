2edccf33eeafaa3735a54c62bf1a134af6cf07c8
ragdoll2025 <ehfjo8@gmail.com>
2026-03-29 21:58:37 +0800

    Set base path for Vite configuration

--- src/aga8-vue/vite.config.ts
+++ src/aga8-vue/vite.config.ts
@@ -4,6 +4,7 @@ import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/aga8-js/',
   plugins: [vue(), githubPagesSpa()],
   optimizeDeps: {
     exclude: ['@sctg/aga8-js']
